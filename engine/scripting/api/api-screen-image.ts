425eb1d6153b4f605f79662d53c45cb1ac89b086
AngryPowman <wenchaoli@tuputech.com>
2017-01-01 08:19:02 +0800

    add result to screen widget api call

--- engine/scripting/api/api-screen-image.ts
+++ engine/scripting/api/api-screen-image.ts
@@ -1,5 +1,8 @@
 import { AVGScriptUnit } from "../script-unit";
 import { ScreenImage } from "../../data/screen-image";
+import { ScriptingResult } from "../../data/scripting-result";
+
+export class ScreenImageResult extends ScriptingResult {}
 
 export class APIScreenImage extends AVGScriptUnit {
     public onClicked: (data: ScreenImage) => void;
--- engine/scripting/api/api-screen-subtitle.ts
+++ engine/scripting/api/api-screen-subtitle.ts
@@ -1,5 +1,8 @@
 import { AVGScriptUnit } from "../script-unit";
 import { Subtitle } from "../../data";
+import { ScriptingResult } from "../../data/scripting-result";
+
+export class ScreenSubtitleResult extends ScriptingResult {}
 
 export class APIScreenSubtitle extends AVGScriptUnit {
     public data: Subtitle = new Subtitle();
