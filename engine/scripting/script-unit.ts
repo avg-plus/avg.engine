a200be2de4460c66f3f59b85b16df8051f83b5e1
AngryPowman <wenchaoli@tuputech.com>
2017-10-05 10:32:56 +0800

    package update

--- engine/scripting/script-unit.ts
+++ engine/scripting/script-unit.ts
@@ -3,5 +3,14 @@ import { AVGData } from "../data/avg-data";
 export type RunnerFunction = (scriptUnit: AVGScriptUnit) => Promise<AVGScriptUnit>;
 export class AVGScriptUnit {
     public data: AVGData;
-    public runner: RunnerFunction;
+    private _runner: RunnerFunction;
+
+    public get runner(): RunnerFunction {
+        console.log('Execute API Runner:', this);
+        return this._runner;
+    }
+
+    public set runner(value: RunnerFunction) {
+        this._runner = value;
+    }
 }
