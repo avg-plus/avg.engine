d0fe60d37f34c3f14728535a3d8ad0d16f61d28e
RealSys <admin@skysy.cn>
2018-05-15 22:33:36 +0800

    Sync some modify.

---
+++ engine/core/state.ts
@@ -0,0 +1,12 @@
+import { ScreenWidget } from "../data/screen-widget";
+
+export class AVGMusicState {
+
+}
+
+export class AVGGameState {
+
+    public screenWidgets: Array<ScreenWidget>;
+
+    
+}
