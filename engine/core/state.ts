c26d696b0e8a26b9a7fbb964bd34b4912639f470
RealSys <admin@skysy.cn>
2018-05-15 23:16:48 +0800

    Update AVGState model.

--- engine/core/state.ts
+++ engine/core/state.ts
@@ -1,12 +1,15 @@
-import { ScreenWidget } from "../data/screen-widget";
-
-export class AVGMusicState {
-
-}
+import { Sound, SoundBGM } from "../data/sound";
+import { Subtitle } from "../data/screen-subtitle";
+import { ScreenImage } from "../data/screen-image";
+import { Avatar } from "../data/avatar";
 
 export class AVGGameState {
 
-    public screenWidgets: Array<ScreenWidget>;
+    public screenSubtitles: Array<Subtitle>;
 
+    public screenImage: Array<ScreenImage>
+
+    public bgm: SoundBGM;
     
+    public avatar: Avatar;
 }
