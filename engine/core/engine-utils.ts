1405f0c5e74705cfee050a745029f9685d2e8e08
AngryPowman <138001655@qq.com>
2018-05-23 21:42:37 +0800

    add css filter parser to EngineUtils

--- engine/core/engine-utils.ts
+++ engine/core/engine-utils.ts
@@ -21,4 +21,68 @@ export class EngineUtils {
 
     return value;
   }
+
+  public static parseCSSFilters(filters: string): Map<string, string> {
+    const container = new Map<string, string>();
+    if (filters === "none") {
+      console.log("No filters found.");
+    } else {
+      const filterArr = filters.split(" ");
+      if (filterArr && filterArr.length > 0) {
+        const regex = /([a-z-]+)\((.*)\)/;
+
+        for (const filter of filterArr) {
+          const matches = filter.match(regex);
+          if (matches && matches.length >= 3) {
+            container.set(matches[1], matches[2]);
+          }
+        }
+      }
+    }
+
+    return container;
+  }
+
+  public static toCSSFilter(container: Map<string, string>): string {
+    let filters = "";
+    container.forEach((v, k) => {
+      filters += k + "(" + v + ") ";
+    });
+
+    return filters;
+  }
+
+  public static countTo(
+    from: number,
+    to: number,
+    duration: number,
+    update: (value: number) => void,
+    done?: () => void
+  ) {
+    const range = to - from;
+    if (range === 0) {
+      return;
+    }
+
+    const increment = to > from ? 0.1 : -0.1;
+    const stepTime = Math.abs(Math.floor(duration / range / 10));
+
+    let current = from;
+
+    const timer = setInterval(() => {
+      current += increment;
+
+      if (update) {
+        update(current);
+      }
+
+      if ((from < to && current >= to) || (from > to && current <= to)) {
+        clearInterval(timer);
+        update(to);
+        if (done) {
+          done();
+        }
+      }
+    }, stepTime);
+  }
 }
