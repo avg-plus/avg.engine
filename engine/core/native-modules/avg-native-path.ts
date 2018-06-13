ea4b0ea54f2669829c5cddad5131616039dafd93
AngryPowman <138001655@qq.com>
2018-06-14 00:03:43 +0800

    Fix bug when path is empty

--- engine/core/native-modules/avg-native-path.ts
+++ engine/core/native-modules/avg-native-path.ts
@@ -7,27 +7,30 @@ export class AVGNativePath {
     // if (PlatformService.isDesktop()) {
     //   return path.join(paths);
     // } else {
-      var parts = [];
-      for (var i = 0, l = paths.length; i < l; i++) {
-        parts = parts.concat(paths[i].split("/"));
+    var parts = [];
+    for (var i = 0, l = paths.length; i < l; i++) {
+      if (!paths[i]) {
+        continue;
       }
-
-      const newParts = [];
-      for (i = 0, l = parts.length; i < l; i++) {
-        const part = parts[i];
-        if (!part) continue;
-        if (part === "..") {
-          newParts.pop();
-        } else {
-          newParts.push(part);
-        }
-      }
-
-      if (parts[0] === "") {
-        newParts.unshift("");
-      }
-
-      return newParts.join("/") || (newParts.length ? "/" : ".");
+      parts = parts.concat(paths[i].split("/"));
     }
+
+    const newParts = [];
+    for (i = 0, l = parts.length; i < l; i++) {
+      const part = parts[i];
+      if (!part) continue;
+      if (part === "..") {
+        newParts.pop();
+      } else {
+        newParts.push(part);
+      }
+    }
+
+    if (parts[0] === "") {
+      newParts.unshift("");
+    }
+
+    return newParts.join("/") || (newParts.length ? "/" : ".");
+  }
   // }
 }
